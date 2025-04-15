dab8f4aca5fec88107f7e365160325a32fca53b0
lopliok <lopliok@gmail.com>
2025-04-15 20:13:31 +0200

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import ExampleComponent from './components/Sample';
+
+
+function App() {
+
+  return (
+    <div className='bg-pastelPurple px-6'>
+       <div className="h-screen flex items-center justify-center bg-pastelPurple">
+      <h1 className="text-3xl font-bold text-black">Test barvy 🎨</h1>
+    </div>
+      <ExampleComponent />
+    </div>
+  )
+}
+
+export default App
