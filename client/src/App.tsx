67b45365bd51decbc2698e0c4df205d417fcb23d
lopliok <lopliok@gmail.com>
2025-04-15 20:16:11 +0200

    code format

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
 import ExampleComponent from './components/Sample';
 
-
 function App() {
-
   return (
-    <div className='bg-pastelPurple px-6'>
-       <div className="h-screen flex items-center justify-center bg-pastelPurple">
-      <h1 className="text-3xl font-bold text-black">Test barvy 🎨</h1>
-    </div>
+    <div className="bg-pastelPurple px-6">
+      <div className="h-screen flex items-center justify-center bg-pastelPurple">
+        <h1 className="text-3xl font-bold text-black">Test barvy 🎨</h1>
+      </div>
       <ExampleComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
